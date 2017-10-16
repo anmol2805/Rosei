b2775a19483159adff2181b37eb0c7dfb73c49c5
anmol2805 <saxenaanmol28@gmail.com>
2017-10-16 14:55:01 +0530

    splash commit

--- app/src/main/java/com/anmol/rosei/SplashActivity.java
+++ app/src/main/java/com/anmol/rosei/SplashActivity.java
@@ -1,13 +1,98 @@
 package com.anmol.rosei;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SplashActivity extends AppCompatActivity {
+    Animation animFadein,zoomin;
+    ImageView img;
+
+    ProgressBar progressBar;
+
+    int key;
+    FirebaseAuth auth;
+    String uid,pwd,urlid,uidu;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("students").child(auth.getCurrentUser().getUid());
+    CircleImageView sanmol,sankit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        sanmol = (CircleImageView)findViewById(R.id.sanmol);
+        sankit = (CircleImageView)findViewById(R.id.sankit);
+        Glide.with(this).load(R.drawable.anmol).into(sanmol);
+        Glide.with(this).load(R.drawable.ankit).into(sankit);
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()==null){
+            startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+        }
+        else{
+//            Intent intent = new Intent(this, RequestService.class);
+//            startService(intent);
+//            mdatabase = FirebaseDatabase.getInstance().getReference().child("Notice");
+//            final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Students").child(auth.getCurrentUser().getUid()).child("hibiscus");
+            progressBar = (ProgressBar)findViewById(R.id.load);
+            progressBar.setVisibility(View.VISIBLE);
+
+            img = (ImageView)findViewById(R.id.imageView2);
+            animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                    R.anim.fade_in);
+            zoomin = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+            img.startAnimation(zoomin);
+            progressBar.setVisibility(View.INVISIBLE);
+            databaseReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if(dataSnapshot!=null && dataSnapshot.child("sid").getValue()!=null && dataSnapshot.child("pwd").getValue()!=null){
+                        uid = dataSnapshot.child("sid").getValue().toString();
+                        pwd = dataSnapshot.child("pwd").getValue().toString();
+//                        uidu = uid.toUpperCase();
+//                        urlid = "https://hib.iiit-bh.ac.in/Hibiscus/docs/iiit/Photos/" + uidu + ".jpg";
+
+                    }
+
+                    Intent intent = new Intent(SplashActivity.this, RoseiActivity.class);
+                    intent.putExtra("url", urlid);
+                    intent.putExtra("uidu", uidu);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.still,R.anim.slide_in_up);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+        overridePendingTransition(R.anim.still,R.anim.slide_in_up);
     }
 }
