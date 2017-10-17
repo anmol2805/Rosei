21a9f2354dd50fd82612acc2d06afbe2f2556346
anmol2805 <saxenaanmol28@gmail.com>
2017-10-17 08:51:05 +0530

    rosei and book started

--- app/src/main/java/com/anmol/rosei/RoseiActivity.java
+++ app/src/main/java/com/anmol/rosei/RoseiActivity.java
@@ -1,13 +1,43 @@
 package com.anmol.rosei;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RoseiActivity extends AppCompatActivity {
 
+    Button book;
+    private static long back_pressed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rosei);
+        book = (Button)findViewById(R.id.book);
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RoseiActivity.this,Book_Activity.class));
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(back_pressed + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+            overridePendingTransition(R.anim.still,R.anim.slide_out_down);
+        }else {
+
+            Toast.makeText(getBaseContext(), "Double tap to exit!", Toast.LENGTH_SHORT).show();
+            back_pressed = System.currentTimeMillis();
+        }
     }
 }
