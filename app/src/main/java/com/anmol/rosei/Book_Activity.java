21c2305a14a23c659759e5c1aef87f7e9aa2a2b3
anmol2805 <saxenaanmol28@gmail.com>
2017-10-19 07:08:25 +0530

    adapter set

--- app/src/main/java/com/anmol/rosei/Book_Activity.java
+++ app/src/main/java/com/anmol/rosei/Book_Activity.java
@@ -2,12 +2,30 @@ package com.anmol.rosei;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Book_Activity extends AppCompatActivity {
-
+    Button m1,m2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        m1 = (Button)findViewById(R.id.m1);
+        m2 = (Button)findViewById(R.id.m2);
+
+        m1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        m2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
