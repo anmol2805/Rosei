6f25e795f547c282f2012eed381035c56fac9df7
anmol2805 <saxenaanmol28@gmail.com>
2017-10-20 14:27:00 +0530

    manu almost done

--- app/src/main/java/com/anmol/rosei/Adapter/Mess1Adapter.java
+++ app/src/main/java/com/anmol/rosei/Adapter/Mess1Adapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,15 +61,67 @@ public class Mess1Adapter extends ArrayAdapter<mess1> {
             foodl.setText(mess1s.get(position).getLnch());
             foodd.setText(mess1s.get(position).getDinnr());
             day.setText(mess1s.get(position).getDay());
-            CheckBox b = (CheckBox)v.findViewById(R.id.b);
-            CheckBox l = (CheckBox)v.findViewById(R.id.l);
-            CheckBox d = (CheckBox)v.findViewById(R.id.d);
+            final CheckBox b = (CheckBox)v.findViewById(R.id.b);
+            final CheckBox l = (CheckBox)v.findViewById(R.id.l);
+            final CheckBox d = (CheckBox)v.findViewById(R.id.d);
             final RadioButton bnv = (RadioButton)v.findViewById(R.id.bnv);
             final RadioButton bv = (RadioButton)v.findViewById(R.id.bv);
             final RadioButton lnv = (RadioButton)v.findViewById(R.id.lnv);
             final RadioButton lv = (RadioButton)v.findViewById(R.id.lv);
             final RadioButton dnv = (RadioButton)v.findViewById(R.id.dnv);
             final RadioButton dv = (RadioButton)v.findViewById(R.id.dv);
+            bnv.setChecked(true);
+            lnv.setChecked(true);
+            dnv.setChecked(true);
+            bnv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        bv.setChecked(false);
+                    }
+                }
+            });
+            bv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        bnv.setChecked(false);
+                    }
+                }
+            });
+            lnv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        lv.setChecked(false);
+                    }
+                }
+            });
+            lv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        lnv.setChecked(false);
+                    }
+                }
+            });
+            dnv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        dv.setChecked(false);
+                    }
+                }
+            });
+            dv.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if(compoundButton.isChecked()){
+                        dnv.setChecked(false);
+                    }
+                }
+            });
+
             b.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
@@ -105,6 +158,42 @@ public class Mess1Adapter extends ArrayAdapter<mess1> {
                     else if(!compoundButton.isChecked()){
                         dnv.setVisibility(View.INVISIBLE);
                         dv.setVisibility(View.INVISIBLE);
+                    }
+                }
+            });
+            CardView brk = (CardView)v.findViewById(R.id.brk);
+            CardView lnch = (CardView)v.findViewById(R.id.lnch);
+            CardView dnnr = (CardView)v.findViewById(R.id.dnnr);
+            brk.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(b.isChecked()){
+                        b.setChecked(false);
+                    }
+                    else if(!b.isChecked()){
+                        b.setChecked(true);
+                    }
+                }
+            });
+            lnch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(l.isChecked()){
+                        l.setChecked(false);
+                    }
+                    else if(!l.isChecked()){
+                        l.setChecked(true);
+                    }
+                }
+            });
+            dnnr.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(d.isChecked()){
+                        d.setChecked(false);
+                    }
+                    else if(!d.isChecked()){
+                        d.setChecked(true);
                     }
                 }
             });
