3f3d170bb32fe31729553aea922f0a4bafbbe0c9
anmol2805 <saxenaanmol28@gmail.com>
2017-10-21 10:37:37 +0530

    roseiui

--- app/src/main/java/com/anmol/rosei/RoseiActivity.java
+++ app/src/main/java/com/anmol/rosei/RoseiActivity.java
@@ -6,10 +6,15 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anmol.rosei.Services.RequestService;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,26 +22,56 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class RoseiActivity extends AppCompatActivity {
     FirebaseAuth auth = FirebaseAuth.getInstance();
     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("students").child(auth.getCurrentUser().getUid()).child("rosei");
-
+    Animation rotate;
     Button book;
+    ImageButton set;
+    TextView stuid;
+    CircleImageView user;
     private static long back_pressed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rosei);
-
-
+        rotate = AnimationUtils.loadAnimation(RoseiActivity.this,R.anim.rotate);
+        set = (ImageButton)findViewById(R.id.set);
         book = (Button)findViewById(R.id.book);
+        user = (CircleImageView)findViewById(R.id.user);
+        stuid = (TextView)findViewById(R.id.stuid);
         book.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(RoseiActivity.this,Book_Activity.class));
             }
         });
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                set.startAnimation(rotate);
+            }
+        });
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("sid").getValue(String.class)!=null){
+                    String sid = dataSnapshot.child("sid").getValue(String.class);
+                    sid = sid.toUpperCase();
+                    stuid.setText(sid);
+                    String url = "https://hib.iiit-bh.ac.in/Hibiscus/docs/iiit/Photos/"+sid+".jpg";
+                    Glide.with(RoseiActivity.this).load(url).into(user);
 
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
     }
