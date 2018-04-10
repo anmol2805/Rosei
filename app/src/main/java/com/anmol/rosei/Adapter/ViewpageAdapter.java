0f748d8839f6480faf3beef278becdc4bcef4a57
anmol2805 <saxenaanmol28@gmail.com>
2018-04-10 13:58:04 +0530

    viewpager calling left

--- app/src/main/java/com/anmol/rosei/Adapter/ViewpageAdapter.java
+++ app/src/main/java/com/anmol/rosei/Adapter/ViewpageAdapter.java
@@ -1,16 +1,51 @@
 package com.anmol.rosei.Adapter;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.anmol.rosei.Model.Coupon;
+import com.anmol.rosei.R;
+
+import java.util.List;
 
 public class ViewpageAdapter extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private List<Coupon> coupons;
+
+    public ViewpageAdapter(Context context, List<Coupon> coupons) {
+        this.context = context;
+        this.coupons = coupons;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return coupons.size();
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View vi = layoutInflater.inflate(R.layout.menulayout,null);
+        ViewPager viewPager = (ViewPager)container;
+        viewPager.addView(vi,0);
+        return vi;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager viewPager = (ViewPager)container;
+        View view = (View)object;
+        viewPager.removeView(view);
     }
 }
