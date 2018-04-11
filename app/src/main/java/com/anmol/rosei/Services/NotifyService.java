86b2302c93b343b31cc321b8f8b247259b446109
anmol2805 <saxenaanmol28@gmail.com>
2018-04-11 11:36:26 +0530

    notification done finally

--- app/src/main/java/com/anmol/rosei/Services/NotifyService.java
+++ app/src/main/java/com/anmol/rosei/Services/NotifyService.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -21,16 +22,26 @@ public class NotifyService extends IntentService {
     protected void onHandleIntent(@Nullable Intent intent) {
         DatabaseReference db = FirebaseDatabase.getInstance().getReference();
         Date date = Calendar.getInstance().getTime();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat adf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat ddf = new SimpleDateFormat("EEEE");
         String day = ddf.format(date);
         System.out.println("todayday:"+day);
         String todaysdate = sdf.format(date);
-        if (day.contains("Wed") || day.contains("wed")){
-            db.child("wednesday").setValue(todaysdate);
+        String weddate = todaysdate + " 20:00:00";
+        String thudate = todaysdate + " 23:00:00";
+        try {
+            Date wed = adf.parse(weddate);
+            Date thu = adf.parse(thudate);
+            if (day.contains("Wed") && date.after(wed)){
+                db.child("wednesday").setValue(todaysdate);
+            }
+            else if(day.contains("Thu") && date.before(thu)){
+                db.child("thursday").setValue(todaysdate);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-        else if(day.contains("Thu") || day.contains("thu")){
-            db.child("thursday").setValue(todaysdate);
-        }
+
     }
 }
