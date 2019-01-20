c1237baad2363cc15a5349e8458fdadd534ddce9
anmol2028 <b516008@iiit-bh.ac.in>
2019-01-20 11:04:10 +0530

    splashactivity queries replaced

--- app/src/main/java/com/anmol/rosei/SplashActivity.java
+++ app/src/main/java/com/anmol/rosei/SplashActivity.java
@@ -13,13 +13,21 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.anmol.rosei.Services.MessStatusService;
 import com.anmol.rosei.Services.NotifyService;
 import com.bumptech.glide.Glide;
+import com.canopydevelopers.canopyauth.AuthConfig;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONObject;
 
 import java.util.Calendar;
 
@@ -28,13 +36,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class SplashActivity extends AppCompatActivity {
     Animation animFadein,zoomin;
     ImageView img;
-
     ProgressBar progressBar;
-
-    int key;
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    String uid,pwd,urlid,uidu;
-    DatabaseReference databaseReference;
     CircleImageView sanmol,sankit;
 
     @Override
@@ -45,26 +47,58 @@ public class SplashActivity extends AppCompatActivity {
         sankit = (CircleImageView)findViewById(R.id.sankit);
         Glide.with(this).load(R.drawable.anmol).into(sanmol);
         Glide.with(this).load(R.drawable.ankit).into(sankit);
-        auth = FirebaseAuth.getInstance();
+        AuthConfig authConfig = new AuthConfig(this);
         //condition to go to loginactivity
-        if(auth.getCurrentUser()==null){
+        if(!authConfig.readloginstatus()){
             startActivity(new Intent(SplashActivity.this,LoginActivity.class));
         }
         //go to roseiactivity or dashboard
         else{
-
-            databaseReference = FirebaseDatabase.getInstance().getReference().child("students").child(auth.getCurrentUser().getUid());
+            img = (ImageView)findViewById(R.id.imageView2);
             progressBar = (ProgressBar)findViewById(R.id.load);
             progressBar.setVisibility(View.VISIBLE);
+            //menu request
+            JsonObjectRequest menurequest = new JsonObjectRequest(Request.Method.POST, "", null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject menuresponse) {
+                    //coupon request
+                    JsonObjectRequest couponrequest = new JsonObjectRequest(Request.Method.POST, "", null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject couponresponse) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Intent intent = new Intent(SplashActivity.this, RoseiActivity.class);
+                            startActivity(intent);
+                            overridePendingTransition(R.anim.still,R.anim.slide_in_up);
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(SplashActivity.this,"Unable to load Coupons",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Mysingleton.getInstance(SplashActivity.this).addToRequestqueue(couponrequest);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    progressBar.setVisibility(View.INVISIBLE);
+                    Toast.makeText(SplashActivity.this,"Unable to load Menu",Toast.LENGTH_SHORT).show();
+                }
+            });
+            Mysingleton.getInstance(this).addToRequestqueue(menurequest);
 
-            img = (ImageView)findViewById(R.id.imageView2);
+
+
+
+
             // animation on splashscreen
             animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
                     R.anim.fade_in);
             zoomin = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
             img.startAnimation(zoomin);
-            Intent intent = new Intent(SplashActivity.this, NotifyService.class);
-            startService(intent);
+//            Intent intent = new Intent(SplashActivity.this, NotifyService.class);
+//            startService(intent);
             Calendar calendar = Calendar.getInstance();
             calendar.set(Calendar.HOUR_OF_DAY, 10);
             calendar.set(Calendar.MINUTE, 59);
@@ -73,19 +107,7 @@ public class SplashActivity extends AppCompatActivity {
             PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,intent1, PendingIntent.FLAG_UPDATE_CURRENT);
             AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
             am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    // service to receive weekly menu
-                    Intent i = new Intent(SplashActivity.this, MessStatusService.class);
-                    startService(i);
-                    progressBar.setVisibility(View.INVISIBLE);
-                    Intent intent = new Intent(SplashActivity.this, RoseiActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.still,R.anim.slide_in_up);
-                }
-            },3000);
+
 
 
 
